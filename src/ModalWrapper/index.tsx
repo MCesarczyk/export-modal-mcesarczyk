44c5c9810ce2edf7b707f4b1c1f8f0abea2c8a3e
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-04-24 04:33:57 +0200

    Modal connected to app

--- src/ModalWrapper/index.tsx
+++ src/ModalWrapper/index.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import Button from "../Button";
+import Modal from "./Modal";
 
 const ModalWrapper = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+
   return (
-    <Button title={"Open modal"} onClick={() => {}} />
+    <>
+      <Button title={"Open modal"} onClick={openModal} />
+      <Modal visible={modalOpen} title={"Export report"} />
+    </>
   );
 };
 
